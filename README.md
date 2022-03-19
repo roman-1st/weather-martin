354d092d8da2633568e314aad81d37f53b42280e
Роман <78566323+roman-1st@users.noreply.github.com>
2022-03-19 18:18:40 +0300

    comit2

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # weather-martin
-weather 
+Задача проекта: получение данных из JSON, обработка и отправка запроса в API, получение данных из API и отображение в HTML
+
+Задачу выполнил, был такой код: 
+
+let search = function() {
+     const massiv = fetch("/city.list.json")
+    .then(function (a) { return a.json() })
+     .then(function (massiv)  {
+         console.log(massiv)
+         viewcity(massiv)
+     })
+}
+function viewcity (city) {
+    let select = document.querySelector("#select")
+    city.forEach(c => {
+        let option = document.createElement('option')
+        c.lenght = option.lenght
+        option.innerHTML = `${c.name}`
+        select.append(option)
+    });
+    
+    select.addEventListener('change', function() {
+        const value = select.value;
+ 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=8f0125d0dac4558db68403baec52a642&units=metric`)
+    .then(function (a) { return a.json() })
+    .then(function (data) {
+        document.querySelector(".city-name").innerHTML = data.name
+        document.querySelector('.status').innerHTML = data.weather[0].description
+        document.querySelector('.humidity').innerHTML = `${data.main.humidity} %`
+        document.querySelector('.temp').innerHTML = ` ${Math.round(data.main.temp)} ℃`
+        document.querySelector('.vision').innerHTML = `${data.visibility} метров` 
+        console.log(data);
+    })
+    })
+
+}
+
+Однако, мне не понравилось, что  JSON, как базы данных городов, хранился на моем ПК, имел ограниченный размер ввиду своего объема. В следсвтие этого я решил отказаться от получения списка городов из JSON в пользу получения названия города непосредственно от пользователя. 
+Плюсом этого решения можно назвать то, что JSON не используется, запрос идет на прямую в API. Также, объем данных с названием городов ограничивается лишь информацией в API, что позволяет искать совершенно любой город или населенный пункт.
+
+Соответственно, для решения вновь поставленной задачи, я принял решение сделать через поисковую строку, чтобы пользователь не выбирал из списка предложенного, а вбивал название города сам. 
+
+Далее, я захотел, чтобы картинки, получаемые с API и картинки-обозначения были одного формата. Подумал и решил получать картинки не через API, а собственные. Около 2х-3х часов подбирал их, но конечный результат мне пришелся почему-то по душе.
+
+В стиле решил отдать предпочтение темным тонам. 
+
+Далее, по получаемой с API информации. Обратил внимание, что все параметры, за исключением видимости, передаются относительно точно. Видимость всегда приходит 10 000, но, несмотря на это решил оставить.
+
+Как итог - относительно забавный погодный одностраничник.
---
+++ script.js
@@ -0,0 +1,97 @@
+// let search = function() {
+//     const massiv = fetch("/city.list.json")
+//     .then(function (a) { return a.json() })
+//     .then(function (massiv)  {
+//         console.log(massiv)
+//         viewcity(massiv)
+//     })
+// }
+
+// search()
+
+let city
+
+function search() {
+    // document.getElementById('btn').addEventListener('click', () => { 
+    // let city = prompt('Введите название города','')
+    // console.log(city)
+    // viewcity (city)
+    // return city
+    // })
+
+    city = document.getElementById('searchcity').value
+    console.log(city);
+    viewcity (city)
+}
+
+
+
+
+function viewcity (city) {
+    // let select = document.querySelector("#select")
+    // city.forEach(c => {
+    //     let option = document.createElement('option')
+    //     c.lenght = option.lenght
+    //     option.innerHTML = `${c.name}`
+    //     select.append(option)
+    // });
+    
+    // select.addEventListener('change', function() {
+    //     const value = select.value;
+ 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=8f0125d0dac4558db68403baec52a642&units=metric`)
+    .then(function (a) { return a.json() })
+    .then(function (data) {
+        if(data.name == undefined) {
+            alert('В названии города ошибка =(')
+        } else { 
+        document.querySelector(".city-name").innerHTML = data.name
+        document.querySelector('.status').innerHTML = data.weather[0].description
+        document.querySelector(".weather-icon").innerHTML = `<img src="/weather-icon/${data.weather[0].icon}.png">`
+        document.querySelector('.humidity').innerHTML = `${data.main.humidity} %`
+        document.querySelector('.temp').innerHTML = ` ${Math.round(data.main.temp)} ℃`
+        document.querySelector('.vision').innerHTML = `${data.visibility} м` 
+        citytime(data)
+        document.querySelector('.wind').innerHTML = `${Math.round(data.wind.speed)} м/с`
+        citywind(data)
+        document.querySelector('.pressure').innerHTML = `${(data.main.pressure / 1.333).toFixed(2)} мм рт.ст.` 
+        document.querySelector('.sky').innerHTML = `Облачность ${data.clouds.all} %`
+        console.log(data);
+        }
+    // })
+    // })
+})
+}
+
+
+let citytime = function (time) {
+    let thisdata = time.dt * 1000
+    let dataD = new Date(thisdata)
+    document.querySelector('.time').innerHTML = `Актуально на ${dataD.getHours()} час. ${dataD.getMinutes()} мин. `
+}
+
+function citywind(num) {
+    let val = Math.floor((num.wind.deg / 22.5) + 0.5);
+    let arr = ["С", "С-СВ", "СВ", "В-СВ", "В", "В-ЮВ", "ЮВ", "Ю-ЮВ", "Ю", "Ю-ЮЗ", "ЮЗ", "З-ЮЗ", "З", "З-СЗ", "С-З", "С-СЗ"];
+    document.querySelector('.wind-compas').innerHTML = `"${arr[(val % 16)]}" ветер` 
+}   
+
+// function citysky(skyes) {
+//     let citiesSky = skyes.clouds.all
+//     if(skyes.clouds.all <= 25 && skyes.clouds.all ==! 0) {
+//         document.querySelector('.----').innerHTML = `Рассеянные облака`
+//     } 
+//     else if (skyes.clouds.all > 25 && skyes.clouds.all <= 50) {
+//         document.querySelector('.----').innerHTML = `Отдельные облака`
+//     }
+//     else if (skyes.clouds.all > 50 && skyes.clouds.all <= 75) {
+//         document.querySelector('.----').innerHTML = `Значительные облака`
+//     }
+//     else if (skyes.clouds.all > 75 && skyes.clouds.all <= 100) {
+//         document.querySelector('.----').innerHTML = `Сплошные облака`
+//     }
+//     else if (skyes.clouds.all == 0) {
+//         document.querySelector('.----').innerHTML = `Ясно`
+//     }
+    
+// }
