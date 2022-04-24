850a25c3fd480cf3d86c6d930c6b2eece5f3a1cb
Роман <78566323+roman-1st@users.noreply.github.com>
2022-04-24 14:57:04 +0300

    add geolocation, update code

--- script.js
+++ script.js
@@ -1,75 +1,83 @@
 document.addEventListener("DOMContentLoaded", () => {
-    search()
-    viewcity (city)
+    search(city)
+    getGeolocation()
 })
 
-let city = 'Санкт-Петербург'
+let city
 
 function search() {
     document.querySelector("#submit").addEventListener("click", () => {
         city = document.getElementById('searchcity').value
         console.log(city);
-        viewcity (city) 
-        viewcitytomottow (city)
+        viewresult (city) 
     })
-
     document.querySelector("#searchcity").addEventListener("keydown", (event) => {
         if(event.keyCode === 13) {
             city = event.target.value
-            viewcity(city)
+            actualapi(city)
         }
     })
 }
 
-function viewcity (city) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=8f0125d0dac4558db68403baec52a642&units=metric`)
+function getGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((v) => {
+            actualapi(null, v.coords)
+        }, (error) => {
+            console.error(error)
+        });
+    } else {
+        console.error("Ваш браузер не поддерживает геолокацию");
+    }
+}
+
+let actualapi = async (city, coords) => {
+    let api = ''
+    if (city) {
+        api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=8f0125d0dac4558db68403baec52a642&units=metric`
+    } else {
+        api = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&lang=ru&appid=8f0125d0dac4558db68403baec52a642&units=metric`
+    }
+    fetch(api)
         .then(function (a) { return a.json() })
         .then(function (data) {
             if(data.name == undefined) {
-            alert('В названии города ошибка =(')
+                alert('В названии города ошибка =(')
             } else { 
                 document.querySelector(".city-name").innerHTML = data.name
-                document.querySelector('.status').innerHTML = data.weather[0].description
+                document.querySelector(".status").innerHTML = data.weather[0].description
                 document.querySelector(".weather-icon").innerHTML = `<img src="weather-icon/${data.weather[0].icon}.png">`
                 document.querySelector('.humidity').innerHTML = `${data.main.humidity} %`
                 document.querySelector('.temp').innerHTML = ` ${Math.round(data.main.temp)} ℃`
-                document.querySelector('.vision').innerHTML = `${data.visibility} м` 
-                citytime(data)
+                document.querySelector('.vision').innerHTML = `${data.visibility}м` 
                 document.querySelector('.wind').innerHTML = `${Math.round(data.wind.speed)} м/с`
-                citywind(data)
                 document.querySelector('.pressure').innerHTML = `${(data.main.pressure / 1.333).toFixed(2)} мм рт.ст.` 
                 document.querySelector('.sky').innerHTML = `облачность ${data.clouds.all} %`
-                console.log(data);
+                citywind(data)
+                citytime(data)
         
                 fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&lang=ru&exclude=current,minutely,hourly,alerts&appid=8f0125d0dac4558db68403baec52a642&units=metric`)
                     .then(function (a) { return a.json() })
                     .then(function (datatomorrow) {
-                    document.querySelector(".status1").innerHTML = datatomorrow.daily[1].weather[0].description
-                    document.querySelector(".pic1").innerHTML = `<img src="weather-icon/${datatomorrow.daily[1].weather[0].icon}.png">`
-                    document.querySelector(".temp1").innerHTML = `Днем ${Math.round(datatomorrow.daily[1].temp.day)} ℃`
-                    document.querySelector(".tempN1").innerHTML = `Ночью ${Math.round(datatomorrow.daily[1].temp.night)} ℃`
 
-                    document.querySelector(".status2").innerHTML = datatomorrow.daily[2].weather[0].description
-                    document.querySelector(".pic2").innerHTML = `<img src="weather-icon/${datatomorrow.daily[2].weather[0].icon}.png">`
-                    document.querySelector(".temp2").innerHTML = `Днем ${Math.round(datatomorrow.daily[2].temp.day)} ℃`
-                    document.querySelector(".tempN2").innerHTML = `Ночью ${Math.round(datatomorrow.daily[2].temp.night)} ℃`
-
-                    document.querySelector(".status3").innerHTML = datatomorrow.daily[3].weather[0].description
-                    document.querySelector(".pic3").innerHTML = `<img src="weather-icon/${datatomorrow.daily[3].weather[0].icon}.png">`
-                    document.querySelector(".temp3").innerHTML = `Днем ${Math.round(datatomorrow.daily[3].temp.day)} ℃`
-                    document.querySelector(".tempN3").innerHTML = `Ночью ${Math.round(datatomorrow.daily[3].temp.night)} ℃`
-
-                    document.querySelector(".status4").innerHTML = datatomorrow.daily[4].weather[0].description
-                    document.querySelector(".pic4").innerHTML = `<img src="weather-icon/${datatomorrow.daily[4].weather[0].icon}.png">`
-                    document.querySelector(".temp4").innerHTML = `Днем ${Math.round(datatomorrow.daily[4].temp.day)} ℃`
-                    document.querySelector(".tempN4").innerHTML = `Ночью ${Math.round(datatomorrow.daily[4].temp.night)} ℃`
+                        function addtohtml (a,b,c,d,e) {
+                            document.querySelector(a).innerHTML = datatomorrow.daily[e].weather[0].description
+                            document.querySelector(b).innerHTML = `<img src="weather-icon/${datatomorrow.daily[e].weather[0].icon}.png">`
+                            document.querySelector(c).innerHTML = `Днем ${Math.round(datatomorrow.daily[e].temp.day)} ℃`
+                            document.querySelector(d).innerHTML = `Ночью ${Math.round(datatomorrow.daily[e].temp.night)} ℃`
+                        }
+                
+                    addtohtml(".status1", ".pic1", ".temp1", ".tempN1", 1)
+                    addtohtml(".status2", ".pic2", ".temp2", ".tempN2", 2)
+                    addtohtml(".status3", ".pic3", ".temp3", ".tempN3", 3)
+                    addtohtml(".status4", ".pic4", ".temp4", ".tempN4", 4)
+                    addtohtml(".status5", ".pic5", ".temp5", ".tempN5", 5)
+                    addtohtml(".status6", ".pic6", ".temp6", ".tempN6", 6)
 
                     document.querySelector('.dayafter').style.display = 'flex'
-
-                    console.log(datatomorrow.daily);
                 })
             }
-    })
+        })
 }
 
 let citytime = function (time) {
